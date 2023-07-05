dd00886cb294bf76f727e589dbeae27e7ed1f181
tartarus-git <niktedig@gmail.com>
2023-07-05 16:30:08 +0200

    Ok so made some changes (including working around two compiler bugs, or at least it very strongly seems like they're compiler bugs), tested a small bit, and the prototype seems to work. I'm gonna give it another look over and then I'll get started on the main part of the engine.

--- moken.h
+++ moken.h
@@ -1,18 +1,24 @@
 #pragma once
 
+#include <cstddef>
+
 namespace moken {
 
 	void report_error(const char *message) noexcept;
 
 	template <typename element_t, size_t array_length>
-	class array_container_t {
-	public:
+	struct array_container_t {
 		using type = element_t;
-		constexpr size_t length = array_length;
+		static constexpr size_t length = array_length;
 
 		element_t data[length];
 
+		consteval array_container_t(const element_t (&source_array)[array_length]) {
+			for (size_t i = 0; i < length; i++) { data[i] = source_array[i]; }
+		}
+
 		consteval element_t& operator[](size_t index) { return data[index]; }
+		consteval const element_t& operator[](size_t index) const { return data[index]; }
 	};
 
 	/*
@@ -25,36 +31,46 @@ namespace moken {
 	};
 	*/
 
+	/*
 	template <auto first_element, decltype(first_element)... trailing_pack>
 	consteval auto value_parameter_pack_to_array_container_t() {
-		array_container_t<decltype(first_element), sizeof...(pack)> result;
+		array_container_t<decltype(first_element), sizeof...(trailing_pack)> result;
 
 		result[0] = first_element;
-		if constexpr (sizeof...(pack) == 0) { return result; }
+		if constexpr (sizeof...(trailing_pack) == 0) { return result; }
 
 		auto trailing_array = value_parameter_pack_to_array_container_t<trailing_pack...>();
 		for (size_t i = 1; i < decltype(result)::length; i++) { result[i] = trailing_array[i - 1]; }
 
 		return result;
 	}
+	*/
 
-	template <char... specification>
+	template <array_container_t specification_container>
 	consteval size_t calculate_max_table_length() {
-		auto spec_array = value_parameter_pack_to_array_container_t<specification>();
+		using spec_element_t = typename decltype(specification_container)::type;
+		// NOTE: The following line of code is a work-around for what seems to be a compiler bug.
+		// I can't write specification_container[i], clang keeps complaining that i is non-const and can't be used in a constant expression, even though this should be an exception since we're using it from inside
+		// a consteval function. I think the compiler must somehow forget the consteval-ness along the way and I think the error is a bug.
+		// Further proof is that with the following line (which just establishes a reference called "specification" to the inner array in specification_container), everything works exactly as intended.
+		// TODO: Something's fishy here, bug report this.
+		const spec_element_t (&specification)[decltype(specification_container)::length] = specification_container.data;
+		constexpr size_t spec_length = decltype(specification_container)::length - 1;	// NOTE: Accounting for null-termination character. We could remove it, but for simplicity's sake, we're leaving it in the actual array, at least for now.
 
 		size_t i = 0;
 		size_t result = 0;
 		bool is_inside_bracket_expression = false;
 
-		auto func_implementation = [&spec_array, &i, &result, &is_inside_bracket_expression, &func_implementation](size_t nesting_depth) consteval {
-			for (; i < decltype(spec_array)::length; i++) {
-				char character = spec_array[i];
+		// NOTE: I wanted to make this consteval instead of constexpr, but for some reason (which smells strongly of a compiler bug, TODO: REPORT!!!!) consteval doesn't work, even though both versions are executed at compile-time in this case.
+		auto func_implementation = [&i, &result, &is_inside_bracket_expression](size_t nesting_depth, const auto& self) constexpr -> void {
+			for (; i < spec_length; i++) {
+				char character = specification[i];
 				switch (character) {
 				case '|':
 					if (i == 0) { report_error(R"(moken spec syntax error: alternation ("|") must be preceeded by something)"); }
-					if (i == decltype(spec_array)::length - 1) { report_error(R"(moken spec syntax error: alternation ("|") must be succeeded by something)"); }
+					if (i == spec_length - 1) { report_error(R"(moken spec syntax error: alternation ("|") must be succeeded by something)"); }
 					if (is_inside_bracket_expression) { report_error(R"(moken spec syntax error: alternation ("|") invalid inside bracket expression)"); }
-					switch (spec_array[i - 1]) {
+					switch (specification[i - 1]) {
 					case '|': report_error(R"(moken spec syntax error: two alternations ("|") cannot be separated by nothing)");
 					case '(': report_error(R"(moken spec syntax error: alternation ("|") cannot be preceeded by an opening parenthesis ("("))");
 					}
@@ -62,7 +78,7 @@ namespace moken {
 				case '*':
 					if (i == 0) { report_error(R"(moken spec syntax error: kleene closure ("*") must be preceeded by something)"); }
 					if (is_inside_bracket_expression) { report_error(R"(moken spec syntax error: kleene closure ("*") invalid inside bracket expression)"); }
-					switch (spec_array[i - 1]) {
+					switch (specification[i - 1]) {
 					case '|': report_error(R"(moken spec syntax error: kleene closure ("*") cannot be preceeded by an alternation ("|"))");
 					case '(': report_error(R"(moken spec syntax error: kleene closure ("*") cannot be preceeded by an opening parenthesis ("("))");
 					}
@@ -73,17 +89,17 @@ namespace moken {
 					break;
 				case ']':
 					if (!is_inside_bracket_expression) { report_error(R"(moken spec syntax error: closing square bracket ("]") is invalid without corresponding opening square bracket ("["))"); }
-					switch (spec_array[i - 1]) {
+					switch (specification[i - 1]) {
 					case '[': report_error(R"(moken spec syntax error: bracket expression ("[...]") cannot be empty)");
 					}
 					is_inside_bracket_expression = false;
 					result++;
 					break;
 				case '(':
-					if (i == decltype(spec_array)::length - 1) { report_error(R"(moken spec syntax error: opening parenthesis ("(") must be succeeded by something)"); }
+					if (i == spec_length - 1) { report_error(R"(moken spec syntax error: opening parenthesis ("(") must be succeeded by something)"); }
 					if (!is_inside_bracket_expression) { report_error(R"~(moken spec syntax error: subexpression ("(...)") invalid inside bracket expression ("[...]"))~"); }
 					i++;
-					func_implementation(nesting_depth + 1);
+					self(nesting_depth + 1, self);
 					break;
 				case ')':
 					if (nesting_depth == 0) { report_error(R"~(moken spec syntax error: closing parenthesis (")") is invalid without corresponding opening parenthesis ("("))~"); }
@@ -92,8 +108,8 @@ namespace moken {
 					return;
 					break;
 				case '\\':
-					if (i == decltype(spec_array)::length - 1) { report_error(R"(moken spec syntax error: escape character ("\") must be succeeded by something)"); }
-					switch (spec_array[i + 1]) {
+					if (i == spec_length - 1) { report_error(R"(moken spec syntax error: escape character ("\") must be succeeded by something)"); }
+					switch (specification[i + 1]) {
 					case '|':
 					case '*':
 					case '[':
@@ -113,7 +129,7 @@ namespace moken {
 				}
 			}
 		};
-		func_implementation(0);
+		func_implementation(0, func_implementation);
 
 		return result;
 	}
@@ -122,12 +138,12 @@ namespace moken {
 		const dfa_table_element_t *next;
 	};
 
-	template <char... specification, size_t table_length = calculate_max_table_length<specification>()>
+	template <array_container_t specification>
 	struct relative_untrimmed_tokenizer_t {
+		static constexpr size_t table_length = calculate_max_table_length<specification>();
 		dfa_table_element_t table[table_length * (unsigned char)-1];
 
 		consteval relative_untrimmed_tokenizer_t() {
-			auto spec_array = value_parameter_pack_to_array_container_t<specification>();
 
 		}
 	};
@@ -140,7 +156,7 @@ namespace moken {
 		tokenizer_t() = delete;
 	};
 
-	template <char... specification>
+	template <array_container_t specification>
 	consteval auto make_tokenizer_t() noexcept {
 		relative_untrimmed_tokenizer_t<specification> relative_untrimmed;
 		return nullptr;	// TODO
---
+++ test.cpp
@@ -0,0 +1,5 @@
+#include "moken.h"
+
+int main() {
+	moken::make_tokenizer_t<"hi there this is valid regex">();
+}
